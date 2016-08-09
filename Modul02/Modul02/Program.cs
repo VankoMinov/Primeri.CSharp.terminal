2d8e2f09056128b5f7ceb7213a9ba801197686dd
Vanko Minov <vanko.minov@gmail.com>
2016-08-09 22:35:17 +0300

    Модул 02 Коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефинирене на променливи
+
+
+			//Събиране на числа
+
+
+
+			//Писане в конзола
+
+
+
+		}
+	}
+}
