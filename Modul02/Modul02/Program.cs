f81638e751d3a3667aed9ae9f4087b138a38359c
Vanko Minov <vanko.minov@gmail.com>
2016-08-10 20:56:57 +0300

    Модул 02 - Осн. оператори за целочислен тип данни

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -7,20 +7,47 @@ namespace Modul02
 		public static void Main (string[] args)
 		{
 			//Дефинирене на променливи
-			int a = 0; int b = 0;
+			int a = 0;
+			int b = 0;
 			int sum = 0;
 
 			//Събиране на числа
-			a = 15; b = 10;
+			a = 10;
+			b = 5;
 			sum = a + b;
 
 
 			//Писане в конзола
 
-			Console.Write (a); Console.Write (" + "); Console.Write (b); Console.Write (" = ");
+			Console.Write (a);
+			Console.Write (" + ");
+			Console.Write (b);
+			Console.Write (" = ");
 
 
 			Console.WriteLine (sum);
+
+			//Допълнителни оператори
+
+			Console.WriteLine (" \n Izpolzvane na += ");
+			a += b; //a = 10 + 5;  a = a+b;
+			Console.WriteLine (a);
+
+			Console.WriteLine (" \n Izpolzvane na -= ");
+			a -= b; //a = 10 - 5;  a = a-b;
+			Console.WriteLine (a);
+
+			Console.WriteLine (" \n Izpolzvane na *= ");
+			a *= b; //a = 10 * 5;  a = a*b;
+			Console.WriteLine (a);
+
+			Console.WriteLine (" \n Izpolzvane na ++ ");
+			a++ ; //a = a+1;
+			Console.WriteLine (a);
+
+			Console.WriteLine (" standartno delene " + (7 / 3).ToString ()  );
+			Console.WriteLine (" ostatuk ot delene " + (7 % 3).ToString ()  );
+
 
 
 		}
