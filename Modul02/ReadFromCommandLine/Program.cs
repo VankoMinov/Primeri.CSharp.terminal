2948f91154833b86e0ed08736e65060e58def5d8
Vanko Minov <vanko.minov@gmail.com>
2016-08-25 23:29:27 +0300

    Четене от команден ред

---
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Definirane na promenlivi
+			int a = 5, b = 0;
+
+			//vavaejdane na vhodni rezultati
+			Console.Write ( "Vavedete a: " );
+			b = Convert.ToInt32 (Console.ReadLine ());
+
+			//kraen rezultat
+			Console.WriteLine ( "\n Rezultata a + b e " +  (a + b).ToString () + "\n\n\n" );
+		}
+	}
+}
