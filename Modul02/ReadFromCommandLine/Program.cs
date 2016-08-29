d6b651b4e9a516b9188b90d6b1e240dcab8ae142
Vanko Minov <vanko.minov@gmail.com>
2016-08-29 22:17:03 +0300

    Rabota sus stringove

--- Modul02/ReadFromCommandLine/Program.cs
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -15,6 +15,21 @@ namespace ReadFromCommandLine
 
 			//kraen rezultat
 			Console.WriteLine ( "\n Rezultata a + b e " +  (a + b).ToString () + "\n\n\n" );
+
+			string test = "1";
+			test += ", 2";
+			test += ", 100";
+			test += ", 4";
+			test += ", 5";
+
+			Console.WriteLine ("Sabirane s += :" + test + "\n");
+			test = test.Replace (",", ";");
+
+			Console.WriteLine ("Rabota s replace" + test + "\n");
+			Console.WriteLine ("Treti znak" + test.Split (';') [2]);
+
+
+
 		}
 	}
 }
